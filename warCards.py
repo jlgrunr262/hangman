e09df0ad88a47247473ec08d3eda8259d28a0300
jlgrunr262 <jlgrunr@gmail.com>
2019-09-14 08:32:17 -0400

    update wtih class Deck

--- warCards.py
+++ warCards.py
@@ -35,3 +35,16 @@ class Card:
         v = self.values[self.value] + " of " \
          + self.suits[self.suit]
         return v
+
+class Deck:
+    def_init_(self):
+        self.cards = []
+        for i in range(2,15):
+            for j in range(4):
+                self.cards.append(Card(i,j))
+        shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return
+        return self.cards.pop()
