1938b9a9a59195840b1fe37effe2a98893f99f18
cnumb262 <jlgrunr@gmail.com>
2019-10-20 16:31:57 -0400

    This War works

--- warCards.py
+++ warCards.py
@@ -1,19 +1,19 @@
 from random import shuffle
 
+
 class Card:
     suits = ["spades", "hearts", "diamonds", "clubs"]
 
     values = [None, None,"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 
-    def _init_(self, v, s):
+    def __init__(self, v, s):
         """suit + value are ints"""
         self.value = v
         self.suit = s
 
-    def _lt_(self, c2):
-        if self.value < c2.valule:
+    def __lt__(self, c2):
+        if self.value < c2.value:
             return True
-
         if self.value == c2.value:
             if self.suit < c2.suit:
                 return True
@@ -21,7 +21,7 @@ class Card:
                 return False
         return False
 
-    def _gt_(self,, c2):
+    def __gt__(self, c2):
         if self.value > c2.value:
             return True
         if self.value == c2.value:
@@ -31,17 +31,20 @@ class Card:
                 return False
         return False
 
-    def _repr_(self):
-        v = self.values[self.value] + " of " \
-         + self.suits[self.suit]
+    def __repr__(self):
+        v = self.values[self.value] +\
+            " of " + \
+            self.suits[self.suit]
         return v
 
+
 class Deck:
-    def _init_(self):
+    def __init__(self):
         self.cards = []
-        for i in range(2,15):
+        for i in range(2, 15):
             for j in range(4):
-                self.cards.append(Card(i,j))
+                self.cards\
+                    .append(Card(i, j))
         shuffle(self.cards)
 
     def rm_card(self):
@@ -50,13 +53,13 @@ class Deck:
         return self.cards.pop()
 
 class Player:
-    def _init_(self):
+    def __init__(self, name):
         self.wins = 0
         self.card = None
         self.name = name
 
 class Game:
-    def _init_(self):
+    def __init__(self):
         name1 = input("p1 name ")
         name2 = input("p2 name ")
         self.deck = Deck()
@@ -77,9 +80,9 @@ class Game:
         cards = self.deck.cards
         print("beginning WAR!")
         while len(cards) >=2:
-            m = "q to quit. Any " + "key to play:"
+            m = "q to quit. Any key to play:"
             response = input(m)
-            if response == 'q'
+            if response == 'q':
                 break
             p1c = self.deck.rm_card()
             p2c = self.deck.rm_card()
@@ -97,12 +100,13 @@ class Game:
 
         print("War is over. {} wins".format(win))
 
-    def winner(slef, p1, p2):
+    def winner(self, p1, p2):
         if p1.wins > p2.wins:
             return p1.name
-        if p1 wins < p2.wins:
+        if p1.wins < p2.wins:
             return p2.name
         return "It was a tie!"
+
 
 game = Game()
 game.play_game()
